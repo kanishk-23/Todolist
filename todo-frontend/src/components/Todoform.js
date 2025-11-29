39587c0e067991f05643bea70e0bce8856a3d4fb
Kanishk Dad <kanishkdad@gmail.com>
2025-11-29 21:06:15 +0530

    updated readme file and some minor updates.

--- todo-frontend/src/components/Todoform.js
+++ todo-frontend/src/components/Todoform.js
@@ -31,7 +31,7 @@ function TodoFormCard({ form, setForm, todos, setTodos, editId, setEditId, setLo
             <input style={{border: '1px solid #575757ff', padding: '10px', marginTop: 20}} placeholder='Start Date' type='date' value={form.start_date} onChange={e => setForm({...form, start_date: e.target.value})}></input>
             <input style={{border: '1px solid #575757ff', padding: '10px', marginTop: 20}} placeholder='Due Date' type='date' value={form.due_date} onChange={e => setForm({...form, due_date: e.target.value})}></input>
             <button style={{border: '1px solid #575757ff', padding: '10px', marginTop: 20 }} type='submit' onClick={handleSubmit}>{editId ? 'Update Task' : 'Add Task'}</button>
-            {editId && (<button onClick={cancelEdit} type='button'>Cancel</button>)}
+            {editId && (<button style={{border: '1px solid #575757ff', padding: '10px', marginTop: 20 }} type='button' onClick={cancelEdit}>Cancel</button>)}
         </div>
     );
 }
